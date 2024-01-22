aeb3357d97eb9ed0faea165924cfb87247e4abe6
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-01-22 01:42:35 +0000

    Refactor

--- app/app.cpp
+++ app/app.cpp
@@ -1,40 +1,177 @@
 #include <Wt/WApplication.h>
 #include <Wt/WBreak.h>
 #include <Wt/WContainerWidget.h>
+#include <Wt/WLabel.h>
 #include <Wt/WLineEdit.h>
 #include <Wt/WPushButton.h>
+#include <Wt/WSelectionBox.h>
+#include <Wt/WTable.h>
+#include <Wt/WTableCell.h>
 #include <Wt/WText.h>
 #include <Wt/WTextArea.h>
 #include <Wt/WTextEdit.h>
 
-#include "ui.hpp"
+#include "emulator.hpp"
+
+using namespace emulator;
+
+class UI : public Wt::WTable {
+public:
+  /*!\brief Instantiate a new form.
+   */
+  UI(void (*)(UI *));
+  // void (*runf)(UI*);
+  void (*on_run_)(UI *);
+
+private:
+  /*!\brief The user selected a new country: adjust the cities combo box.
+   */
+  void countryChanged();
+
+  /*!\brief Submit the form.
+   */
+  void submit();
+
+  void create();
+
+  Wt::WContainerWidget *feedbackMessages_;
+
+  Wt::WTextArea *remarksEdit_;
+
+  /*!\brief Add a validation feedback for a field
+   */
+  void addValidationStatus(int row, Wt::WFormWidget *field);
+
+  /*!\brief Validate the form, and return whether succesfull.
+   */
+  bool validate();
+
+  /*!\brief Validate a single form field.
+   *
+   * Checks the given field, and appends the given text to the error
+   * messages on problems.
+   */
+  bool checkValid(Wt::WFormWidget *edit, const Wt::WString &text);
+};
+
+UI::UI(void (*f)(UI *)) : on_run_(f), WTable() { create(); }
+
+void UI::create() {
+  Wt::WLabel *label;
+  int row = 0;
+
+  // Title
+  elementAt(row, 0)->setColumnSpan(3);
+  elementAt(row, 0)->setContentAlignment(Wt::AlignmentFlag::Top |
+                                         Wt::AlignmentFlag::Center);
+  elementAt(row, 0)->setPadding(10);
+  Wt::WText *title = elementAt(row, 0)->addWidget(
+      std::make_unique<Wt::WText>(tr("example.form")));
+  title->decorationStyle().font().setSize(Wt::FontSize::XLarge);
+
+  // error messages
+  ++row;
+  elementAt(row, 0)->setColumnSpan(3);
+  feedbackMessages_ = elementAt(row, 0);
+  feedbackMessages_->setPadding(5);
+
+  Wt::WCssDecorationStyle &errorStyle = feedbackMessages_->decorationStyle();
+  errorStyle.setForegroundColor(Wt::WColor("red"));
+  errorStyle.font().setSize(Wt::FontSize::Smaller);
+  errorStyle.font().setWeight(Wt::FontWeight::Bold);
+  errorStyle.font().setStyle(Wt::FontStyle::Italic);
+
+  ++row;
+  remarksEdit_ =
+      elementAt(row, 2)->addWidget(std::make_unique<Wt::WTextArea>());
+  remarksEdit_->setColumns(40);
+  remarksEdit_->setRows(5);
+  label = elementAt(row, 0)->addWidget(
+      std::make_unique<Wt::WLabel>(tr("example.remarks")));
+  label->setBuddy(remarksEdit_);
+
+  // Submit
+  ++row;
+  Wt::WPushButton *submit = elementAt(row, 0)->addWidget(
+      std::make_unique<Wt::WPushButton>(tr("submit")));
+  submit->clicked().connect(this, &UI::submit);
+  submit->setMargin(15, Wt::Side::Top);
+  elementAt(row, 0)->setColumnSpan(3);
+  elementAt(row, 0)->setContentAlignment(Wt::AlignmentFlag::Top |
+                                         Wt::AlignmentFlag::Center);
+
+  // Set column widths for label and validation icon
+  elementAt(2, 0)->resize(Wt::WLength(30, Wt::LengthUnit::FontEx),
+                          Wt::WLength::Auto);
+  elementAt(2, 1)->resize(20, Wt::WLength::Auto);
+}
+
+bool UI::checkValid(Wt::WFormWidget *edit, const Wt::WString &text) {
+  if (edit->validate() != Wt::ValidationState::Valid) {
+    feedbackMessages_->addWidget(std::make_unique<Wt::WText>(text));
+    feedbackMessages_->addWidget(std::make_unique<Wt::WBreak>());
+    edit->label()->decorationStyle().setForegroundColor(Wt::WColor("red"));
+    edit->setStyleClass("Wt-invalid");
+    return false;
+  } else {
+    edit->label()->decorationStyle().setForegroundColor(Wt::WColor());
+    edit->setStyleClass("");
+
+    return true;
+  }
+}
+
+bool UI::validate() {
+  feedbackMessages_->clear();
+  bool valid = true;
+
+  return valid;
+}
+
+void UI::submit() {
+  if (validate()) {
+    // do something useful with the data...
+    // on_run(this);
+
+    Wt::WString remarks = remarksEdit_->text();
+    on_run_(this);
+
+    if (!remarks.empty()){
+      elementAt(0, 0)->addWidget(std::make_unique<Wt::WText>(
+          Wt::WString("<p>You had some remarks. Splendid !</p>")));
+    }
+
+    // wApp->quit();
+  }
+}
+
+
 class RiscvOnline : public Wt::WContainerWidget {
 public:
   RiscvOnline();
+  emulator::Emulator emu;
 
 private:
   Wt::WTextArea *programEdit_;
-  Wt::WText *greeting_;
+};
+
+static void UIrun(UI *ui){
+    // emu.run();
+    // update UI
+    /* ui.update()  */
 };
 
 RiscvOnline::RiscvOnline() : WContainerWidget() {
-  UI* ui = this->addWidget(std::make_unique<UI>());
+  UI *ui = this->addWidget(std::make_unique<UI>(&UIrun));
   ui->setMargin(20);
 }
-
-// int main(int argc, char **argv)
-// {
-//     return Wt::WRun(argc, argv, [](const Wt::WEnvironment& env) {
-//       return std::make_unique<RiscvOnline>(env);
-//     });
-// }
 
 std::unique_ptr<Wt::WApplication>
 createApplication(const Wt::WEnvironment &env) {
   std::unique_ptr<Wt::WApplication> app =
       std::make_unique<Wt::WApplication>(env);
   app->messageResourceBundle().use(Wt::WApplication::appRoot() + "app");
-  app->setTitle("R-V Online");
+  app->setTitle("Risc-V OL");
 
   app->root()->addWidget(std::make_unique<RiscvOnline>());
 
