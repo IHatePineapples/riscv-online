a2e1b28ac4de3f6a700c87079240db66bcc72c5c
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-03-28 02:17:44 +0000

    Add small logger utility header

---
+++ src/error.hpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "pico/stdio.h"
+#include "pico/cyw43_arch.h"
+
+/**
+ * \file error.hpp
+ * Defines some debug helpers that will print to USB,
+ * useful crashing or core-ing.
+*/
+
+#ifdef NDEBUG
+
+/**
+ * \fn void __printf_and_abort(const char* f, const int i)
+ * 
+ * \brief Dump some debug info and crash.
+ * \param f a C string for error/crash location.
+ * \param i a line number to be more specific on location
+ * \note Call this, and program flow stops, calling abort after logging. 
+ *  Keep for desperate unrecoverable states.
+*/
+void __printf_and_abort(const char* f, const int i){
+    printf("%s:%d: Aborting now.");
+    abort();
+}
+/**
+ * \def on_error()
+ * Effective only in Debug build.
+ * Wraps around __PRETTY_FUNCTION and __LINE__ and calls __printf_and_abort.
+ * See lower for Release definition
+ * 
+*/
+#   define on_error() __printf_and_abort((__PRETTY_FUNCTION__), (__LINE__))
+#else
+/**
+ * \def on_error()
+ * In Release mode: abort silently.
+*/
+#   define on_error() abort()
+#endif
