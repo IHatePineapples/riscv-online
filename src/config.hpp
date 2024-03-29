15326c98ff2d64536c95a2641ae89d44cf86db45
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-03-29 15:02:49 +0000

    Add a config.hpp file

---
+++ src/config.hpp
@@ -0,0 +1,18 @@
+/**
+ * \file config.hpp Configuration for things such as Wi-Fi name and password
+ * 
+ * \warning You must edit this. The default values are bogus.
+*/
+
+/**
+ * \def wifi_ssid The Wi-Fi name, you need to edit these as the defaults are bogus.
+ * \warning Edit this to match a real Wi-Fi.
+ */
+#define wifi_ssid "QA"
+
+/**
+ * \def wifi_passwd The Wi-Fi password, you need to edit these as the defaults are bogus.
+ * \warning Edit this to match a real Wi-Fi.
+ */
+#define wifi_passwd "RISCV-Online"
+
