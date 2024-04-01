d991cf1ab681a1cb6ff0abeffe1bf1daa4355f48
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-04-01 10:22:06 +0100

    Implement http server startup

--- src/server.cpp
+++ src/server.cpp
@@ -1,16 +1,26 @@
 #include "lwip/apps/httpd.h"
 
 #include "server.hpp"
-#include "ssi.h"
+// #include "ssi.h"
+#include "cgi.h"
+#include "pico/cyw43_arch.h"
+#include "pico/time.h"
 
 namespace server
 {
     void init_all()
     {
         httpd_init();
-        ssi_init();
+        // ssi_init();
+        cgi_init();
     }
+
     void run(){
         init_all();
+        for (;;){
+            printf("%s:%d: Polling\n", __PRETTY_FUNCTION__, __LINE__);
+            cyw43_arch_poll();
+            sleep_ms(200);
+        }
     }
 }
