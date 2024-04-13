75582c6ede3a217f2b41c62681a6a1e541184ed5
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-04-13 23:27:47 +0100

    Add prototypes

--- src/emulator.hpp
+++ src/emulator.hpp
@@ -11,7 +11,7 @@ namespace emulation
 
   void run();
 
-  template<int xlen>
+  template <int xlen = 32>
   struct emulator final
   {
 
@@ -51,12 +51,230 @@ namespace emulation
 
     reg pc;
 
-    std::vector<std::string> asm_lines_;
-    parse::symbol_table_t symbols_;
-    parse::data_t data_;
-    parse::text_t asm_;
+    std::vector<std::bitset<xlen>> RAM;
 
-    emulator() = default;
+    /**
+     * \fn lui_
+     *
+     * \brief Load Unsigned Immediate.
+     */
+    void lui_();
+    /**
+     * \fn auipc_
+     *
+     * \brief Add Upper Immeddiate to PC.
+     */
+    void auipc_();
+    /**
+     * \fn jal_
+     *
+     * \brief Jump And Link.
+     */
+    void jal_();
+    /**
+     * \fn jalr_
+     *
+     * \brief Jump And Link Register.
+     */
+    void jalr_();
+    /**
+     * \fn beq_
+     *
+     * \brief Branch EQual.
+     */
+    void beq_();
+    /**
+     * \fn bne_
+     *
+     * \brief Branch Not Equal.
+     */
+    void bne_();
+    /**
+     * \fn blt_
+     *
+     * \brief Branch Less Than.
+     */
+    void blt_();
+    /**
+     * \fn bge_
+     *
+     * \brief Branch Greater than or Equal.
+     */
+    void bge_();
+    /**
+     * \fn bltu_
+     *
+     * \brief Branch Less Than Unsigned.
+     */
+    void bltu_();
+    /**
+     * \fn bgeu_
+     *
+     * \brief Branch Greater than or Equal Unsigned.
+     */
+    void bgeu_();
+    /**
+     * \fn lb_
+     *
+     * \brief Load Byte.
+     */
+    void lb_();
+    /**
+     * \fn lh_
+     *
+     * \brief Load Halfword.
+     */
+    void lh_();
+    /**
+     * \fn lw_
+     *
+     * \brief Load Word.
+     */
+    void lw_();
+    /**
+     * \fn lbu_
+     *
+     * \brief Load Byte Unsigned.
+     */
+    void lbu_();
+    /**
+     * \fn lhu_
+     *
+     * \brief Load Halfword Unsigned.
+     */
+    void lhu_();
+    /**
+     * \fn sb_
+     *
+     * \brief Store Byte.
+     */
+    void sb_();
+    /**
+     * \fn sh_
+     *
+     * \brief Store Halfword.
+     */
+    void sh_();
+    /**
+     * \fn sw_
+     *
+     * \brief Store Word.
+     */
+    void sw_();
+    /**
+     * \fn addi_
+     *
+     * \brief Add Immediate.
+     */
+    void addi_();
+    /**
+     * \fn slti_
+     *
+     * \brief Set Less Than Immediate.
+     */
+    void slti_();
+    /**
+     * \fn sltiu_
+     *
+     * \brief Set Less Than Immediate Unsigned.
+     */
+    void sltiu_();
+    /**
+     * \fn xori_
+     *
+     * \brief eXclusive OR Immediate.
+     */
+    void xori_();
+    /**
+     * \fn ori_
+     *
+     * \brief OR Immediate.
+     */
+    void ori_();
+    /**
+     * \fn andi_
+     *
+     * \brief AND Immediate.
+     */
+    void andi_();
+    /**
+     * \fn slli_
+     *
+     * \brief Shift Left Logical Immediate.
+     */
+    void slli_();
+    /**
+     * \fn stli
+     *
+     * \brief Shift Right Logical Immediate.
+     */
+    void srli_();
+    /**
+     * \fn srai_
+     *
+     * \brief Shift Right Arithmetic Immediate.
+     */
+    void srai_();
+    /**
+     * \fn add_
+     *
+     * \brief ADD.
+     */
+    void add_();
+    /**
+     * \fn sub_
+     *
+     * \brief SUBstract.
+     */
+    void sub_();
+    /**
+     * \fn sll_
+     *
+     * \brief Shift Left Logical.
+     */
+    void sll_();
+    /**
+     * \fn slt_
+     *
+     * \brief Set Less Than.
+     */
+    void slt_();
+    /**
+     * \fn sltu_
+     *
+     * \brief Set Less Than Unsigned.
+     */
+    void sltu_();
+    /**
+     * \fn xor_
+     *
+     * \brief eXclusive OR.
+     */
+    void xor_();
+    /**
+     * \fn srl_
+     *
+     * \brief Shift Right Logical.
+     */
+    void srl_();
+    /**
+     * \fn sra_
+     *
+     * \brief Shift Right Arithmetic.
+     */
+    void sra_();
+    /**
+     * \fn or_
+     *
+     * \brief OR.
+     */
+    void or_();
+    /**
+     * \fn and_
+     *
+     * \brief AND.
+     */
+    void and_();
 
   };
 
