cf741fb8377b21004fd12e367bd718020ceaf3e1
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-04-26 14:06:58 +0100

    Fixup signedness

--- src/test/test_emulator_add.cpp
+++ src/test/test_emulator_add.cpp
@@ -5,10 +5,18 @@
 #undef NDEBUG
 #include <boost/assert.hpp>
 
-inline unsigned long operator*(emulation::reg bs)
+inline long operator*(const emulation::reg bs)
 {
-    return bs.to_ulong();
+    
+    int32_t representation = 0;
+    for (std::size_t i = 0; i < bs.size() - 2; ++i)
+      representation += bs[i] << i;
+
+    representation -= bs[bs.size() - 1] << bs.size() - 2;
+    std::cout << representation << std::endl;
+    return representation;
 }
+
 int main()
 {
     emulation::emulator emu{};
@@ -46,8 +54,8 @@ int main()
 
     BOOST_VERIFY(*l + *r == *res);
     std::cout << *l << " + " << *r << " = " << *res << std::endl;
-    l = 514141415;
-    r = 202323230;
+    l = -202,323,230;
+    r = 514141415;
     emu.add_(res, l, r);
 
     BOOST_VERIFY(*l + *r == *res);
