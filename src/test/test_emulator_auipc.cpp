ca4049391743d2ebcc0b9e9a49b8803491b21e99
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-04-26 19:41:40 +0100

    Test auipc

--- src/test/test_emulator_auipc.cpp
+++ src/test/test_emulator_auipc.cpp
@@ -1,1 +1,31 @@
-int main(){}
+
+#include <iostream>
+
+#include "RoL/emulation/emulator.hpp"
+
+#undef NDEBUG
+#include <boost/assert.hpp>
+
+int main()
+{
+    /** \bug `auipc` doesn't work! At all. */
+    emulation::emulator emu{};
+
+    for (std::size_t n; n < 5000; ++n)
+    {
+        emulation::reg pre;
+        emu.auipc_(pre, 0);
+
+        const int offset = rand();
+
+        emulation::reg post;
+        emu.auipc_(post, 0);
+        std::cout << pre << '\n';
+        std::cout << '+' << '\n';
+        std::cout << std::bitset<20>(offset) << "oooooooooooo" << '\n';
+        std::cout << '=' << '\n';
+        std::cout << post << '\n';
+        std::cout << std::endl;
+        BOOST_VERIFY(post.to_ulong() == pre.to_ulong() + offset);
+    }
+}
