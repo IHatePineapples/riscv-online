a849037a853c9e8f570e8ddbcaf7f4b2ece6e506
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-04-30 23:00:45 +0100

    Finish implementation of SSI

--- src/ssi.cpp
+++ src/ssi.cpp
@@ -154,7 +154,7 @@ u16_t ssi_handler(int iIndex, char *pcInsert, int iInsertLen)
         printed = snprintf(pcInsert, iInsertLen, state.pc.to_string().c_str());
         break;
     case 34:
-        printed = snprintf(pcInsert, iInsertLen, "" /** \todo Get Program from ram */);
+        printed = snprintf(pcInsert, iInsertLen, state.serialize_ram().c_str());
         break;
     default:
         break;
