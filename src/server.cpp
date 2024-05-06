fa9c8a1fb1571efa195b5db2ddbf3773c0497c34
Pineapples <91441379+IHatePineapples@users.noreply.github.com>
2024-05-06 12:01:20 +0100

    Be free, my polling thread

--- src/server.cpp
+++ src/server.cpp
@@ -3,7 +3,6 @@
 #include "lwip/apps/httpd.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
-#include "pico/time.h"
 
 #include "RoL/threaded/shared.hpp"
 #include "ssi.h"
@@ -22,7 +21,6 @@ namespace server
         for (;;)
         {
             cyw43_arch_poll();
-            sleep_ms(50);
         }
     }
 }
